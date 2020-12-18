6c8c1c9e8d5900f010940cf5654e8372448efccd
quilteno <14162564437@qq.com>
2020-12-18 22:15:10 +0800

    测试没有双引号

--- sudoku.c
+++ sudoku.c
@@ -3,10 +3,12 @@
 void f(int *p);
 int main(){
     int a=9;
+    int *p=&a;
+printf("*p=%d\n",*p);
     f(&a);
     return 0;
 }
 
 void f(int *p){
-    printf("*p=%d",p[0]);
+    printf("*p=%d\n",p[0]);
 }
